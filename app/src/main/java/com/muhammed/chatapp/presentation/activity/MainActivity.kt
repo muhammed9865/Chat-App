9fd22222a292c33a77138cca76e0d945d057f994
Muhammed Salman <84887514+muhammed9865@users.noreply.github.com>
2022-05-20 16:26:11 +0200

    new: setup bottom navigation

--- app/src/main/java/com/muhammed/chatapp/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/muhammed/chatapp/presentation/activity/MainActivity.kt
@@ -2,24 +2,46 @@ package com.muhammed.chatapp.presentation.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.navigation.NavigationBarView
 import com.google.android.material.shape.CornerFamily
 import com.google.android.material.shape.MaterialShapeDrawable
 import com.muhammed.chatapp.R
 import com.muhammed.chatapp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val navController: NavController by lazy {
+        val host = supportFragmentManager.findFragmentById(R.id.fragmentsContainer) as NavHostFragment
+        host.navController
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        makeBotNavRoundedCorners()
+        binding.botNav.setOnItemSelectedListener(this)
+
+    }
+
+    private fun makeBotNavRoundedCorners() {
         val radius = resources.getDimension(R.dimen.bot_nav_corners)
-        val shapeDrawable: MaterialShapeDrawable = binding.botNav.background as MaterialShapeDrawable
+        val shapeDrawable: MaterialShapeDrawable = binding.botBar.background as MaterialShapeDrawable
         shapeDrawable.shapeAppearanceModel = shapeDrawable.shapeAppearanceModel.toBuilder()
             .setTopRightCorner(CornerFamily.ROUNDED, radius)
             .setTopLeftCorner(CornerFamily.ROUNDED, radius)
             .build()
+    }
 
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.chats -> navController.navigate(R.id.chatsFragment)
+            R.id.calls -> navController.navigate(R.id.callsFragment)
+            R.id.community -> navController.navigate(R.id.communitiesFragment)
+        }
 
+        return true
     }
 }
---
+++ app/src/main/java/com/muhammed/chatapp/presentation/fragments/CommunitiesFragment.kt
@@ -0,0 +1,20 @@
+package com.muhammed.chatapp.presentation.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.muhammed.chatapp.R
+
+
+class CommunitiesFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_communities, container, false)
+    }
+
+}
