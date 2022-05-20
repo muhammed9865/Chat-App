454922b064eb0e59e7036c682de5f4046ede19f9
Muhammed Salman <84887514+muhammed9865@users.noreply.github.com>
2022-05-20 10:09:56 +0200

    add: setup the project dependecies

--- app/src/main/java/com/muhammed/chatapp/MainActivity.kt
+++ app/src/main/java/com/muhammed/chatapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.muhammed.chatapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.shape.CornerFamily
+import com.google.android.material.shape.EdgeTreatment
+import com.google.android.material.shape.MaterialShapeDrawable
+import com.muhammed.chatapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
